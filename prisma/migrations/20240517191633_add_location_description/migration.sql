638483b405ceb188255b73bcd75cf49928620124
Witnney <witnneygabriel13@gmail.com>
2024-05-17 16:34:22 -0300

    feat: add location description

---
+++ prisma/migrations/20240517191633_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium consectetur autem quo exercitationem et tenetur, accusamus, modi, tempora numquam quidem pariatur deserunt. Explicabo quidem fuga laboriosam libero minima ducimus dicta?';
