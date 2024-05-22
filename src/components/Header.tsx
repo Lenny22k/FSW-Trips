f67c8915db31d6fa066ad9861e2adf94f8e0c2d8
Witnney <witnneygabriel13@gmail.com>
2024-05-22 16:16:49 -0300

    feat: fix the my trip button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -54,10 +54,7 @@ export function Header() {
           {menuIsOpen && (
             <div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
               <Link href={"/my-trips"}>
-                <button
-                  className="text-primary pb-2 border-b border-grayLighter border-solid text-xs font-semibold"
-                  onClick={handleLogoutClick}
-                >
+                <button className="text-primary pb-2 border-b border-grayLighter border-solid text-xs font-semibold">
                   Minhas viagens
                 </button>
               </Link>
