5bc60ced53ec3ab7bec94f4426a887570be41ea6
Witnney <witnneygabriel13@gmail.com>
2024-05-22 16:49:42 -0300

    feat: add user reservation page

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { TripReservation } from "@prisma/client";
+import Button from "@/components/Button";
+import { Prisma, TripReservation } from "@prisma/client";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { UserReservationItem } from "./container/UserReservationItem";
 
 export default function MyTrips() {
-  const [reservations, setReservations] = useState<TripReservation[]>([]);
+  const [reservations, setReservations] = useState<
+    Prisma.TripReservationGetPayload<{
+      include: { trip: true };
+    }>[]
+  >([]);
 
   const router = useRouter();
 
@@ -28,7 +35,34 @@ export default function MyTrips() {
     };
 
     fetchReservation();
-  }, [status]);
+  }, [status, router, data?.user]);
 
-  return <div>oi</div>;
+  return (
+    <div className="container mx-auto p-5">
+      <h1 className="font-semibold text-primaryDarker text-xl ">
+        Minhas Viagens
+      </h1>
+      {reservations.length > 0 ? (
+        <div className="flex flex-col">
+          {reservations?.map((reservation) => (
+            <UserReservationItem
+              // fetchReservations={fetchReservations}
+              key={reservation.id}
+              reservation={reservation}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col ">
+          <p className="mt-2 font-medium text-primaryDarker">
+            Você ainda não tem nenhuma reserva! =(
+          </p>
+
+          <Link href="/">
+            <Button className="w-full mt-2 ">Fazer reserva</Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
