f8896ae0a6e4e7876988c2a485ea8b5e549d7332
Witnney <witnneygabriel13@gmail.com>
2024-05-22 17:18:13 -0300

    chore: fix reservations request

--- src/app/api/user/[userId]/reservations/route.ts
+++ src/app/api/user/[userId]/reservations/route.ts
@@ -1,11 +1,10 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET({
-  params: { userId },
-}: {
-  params: { userId: string };
-}) {
+export async function GET(
+  request: Request,
+  { params: { userId } }: { params: { userId: string } }
+) {
   if (!userId) {
     return {
       status: 400,
